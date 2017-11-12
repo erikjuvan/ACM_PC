7fb76956fc27688c30053a097b421bf00218700a
erik.juvan <erik.juvan@mojmail.com>
2017-11-12 22:03:16 +0100

    doing multithreading optimizations

--- include/FFT.h
+++ include/FFT.h
@@ -7,16 +7,22 @@
 #include <algorithm>
 #include <iomanip>
 #include <chrono>
+#include <thread>
+#include <mutex>
 
 class FFT {
 private:
 
 	struct FFTStruct {
+		static int structCnt;
+
 		double* dataIn;
 		std::complex<double>* dataOut;
 		std::vector<fftw_plan> fftPlan;
+		std::thread	thread_;
 
-		FFTStruct(int bufSize) {
+		FFTStruct(int bufSize) : thread_(&threadCompute, this, structCnt) {
+			structCnt++;
 			dataIn = new double[bufSize]();
 			dataOut = new std::complex<double>[bufSize]();
 			fftPlan.push_back(fftw_plan_dft_r2c_1d(bufSize, dataIn, reinterpret_cast<fftw_complex*>(dataOut), FFTW_MEASURE));
@@ -33,11 +39,17 @@ private:
 	std::vector<FFTStruct> fftChannels_;
 	int numOfChannels_;
 	int chBufSize_;
-	int sampleFreq_;	
+	int sampleFreq_;
+	// Multi-threading
+	bool runThread_;
+	int threadCpltCount_;
+	std::mutex cpltMutex;
 
 public:
 
-	FFT(int numOfCh, int sampleFreq, int bufSize) : numOfChannels_(numOfCh), sampleFreq_(sampleFreq), chBufSize_(bufSize) {
+	// const uint8_t* buf, std::vector<double>& freq, std::vector<double>& ampl, std::vector<int>& num_cycles
+	FFT(int numOfCh, int sampleFreq, int bufSize) : numOfChannels_(numOfCh), sampleFreq_(sampleFreq), chBufSize_(bufSize), 
+	 runThread_(false), threadCpltCount_(0) {
 		fftChannels_.reserve(numOfCh);
 		for (int i = 0; i < numOfCh; i++)
 			fftChannels_.emplace_back(bufSize);
@@ -56,35 +68,47 @@ public:
 				fftChannels_[chi].fftPlan.push_back(fftw_plan_dft_r2c_1d(chBufSize_ - i, fftChannels_[chi].dataIn, reinterpret_cast<fftw_complex*>(fftChannels_[chi].dataOut), FFTW_MEASURE));	
 	}
 
-	void run(const uint8_t* buf, std::vector<double>& freq, std::vector<double>& ampl, std::vector<int>& num_cycles) {
-		for (int ci = 0; ci < numOfChannels_; ++ci) {
-			// Setup data
-			for (int i = 0; i < chBufSize_; ++i) {
-				fftChannels_[ci].dataIn[i] = static_cast<double>(*(buf + i * numOfChannels_ + ci));
-			}
+	void threadCompute(FFTStruct* st, int ci) {
+		while (true) {
+			if (runThread_) {
 
-			int optimizeCnt = 0;
-			double maxVal = 0.0;
-			int idx = 0;
-			int size = chBufSize_;
-			for (auto plan : fftChannels_[ci].fftPlan) {
-				fftw_execute(plan);
-				int tmpSize = chBufSize_ - optimizeCnt;
-				optimizeCnt++;
-				std::complex<double>* newMaxVal = std::max_element(fftChannels_[ci].dataOut + 1, fftChannels_[ci].dataOut + tmpSize / 4,	// discard 0 index (DC offset) and search only the first quarter
-					[](std::complex<double> const & lhs, std::complex<double> const & rhs) { return std::abs(lhs) < std::abs(rhs); });				
+				// Setup data
+				for (int i = 0; i < chBufSize_; ++i) {
+					st->dataIn[i] = static_cast<double>(*(buf + i * numOfChannels_ + ci));
+				}
 
-				if (std::abs(*newMaxVal) > maxVal) {
-					maxVal = std::abs(*newMaxVal);
-					idx = std::distance(fftChannels_[ci].dataOut, newMaxVal);
-					size = tmpSize;
+				int optimizeCnt = 0;
+				double maxVal = 0.0;
+				int idx = 0;
+				int size = chBufSize_;
+				for (auto plan : st->fftPlan) {
+					fftw_execute(plan);
+					int tmpSize = chBufSize_ - optimizeCnt;
+					optimizeCnt++;
+					std::complex<double>* newMaxVal = std::max_element(st->dataOut + 1, st->dataOut + tmpSize / 4,	// discard 0 index (DC offset) and search only the first quarter
+						[](std::complex<double> const & lhs, std::complex<double> const & rhs) { return std::abs(lhs) < std::abs(rhs); });
+
+					if (std::abs(*newMaxVal) > maxVal) {
+						maxVal = std::abs(*newMaxVal);
+						idx = std::distance(st->dataOut, newMaxVal);
+						size = tmpSize;
+					}
+				}
+
+				// Return values
+				freq[ci] = (double)(idx * sampleFreq_) / (double)size;
+				ampl[ci] = maxVal * 2 / size;
+				num_cycles[ci] = static_cast<int>(idx);
+
+				std::lock_guard<std::mutex> lock(cpltMutex);
+				if (++threadCpltCount_ >= numOfChannels_) {
+					runThread_ = false;
 				}
 			}
-
-			// Return values
-			freq[ci] = (double)(idx * sampleFreq_) / (double)size;
-			ampl[ci] = maxVal * 2 / size;
-			num_cycles[ci] = static_cast<int>(idx);
 		}
+	}
+	
+	void run() {
+		runThread_ = true;
 	}
 };
